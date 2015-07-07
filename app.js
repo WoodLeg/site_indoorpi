77dd9b8dcd3ec46525e702af8a35503d1ba61c1e
Paul Souvestre <paul.souvestre@gmail.com>
2015-07-07 15:07:14 +0200

    Add main route

--- app.js
+++ app.js
@@ -2,18 +2,13 @@ var express = require('express');
 var app = express();
 var morgan = require('morgan');
 
+var mainRoute = require('./routes/mainRoute');
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
-
-
-
-app.get('/', function(request, response){
-      response.sendFile('index.html');
-});
-
-
+// Routing
+app.use('/', mainRoute);
 
 var port = process.env.port || 8080;
 app.listen(port, function(){
